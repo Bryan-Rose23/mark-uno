f54e0551f5f608f6b78e0ac825f61acdc9068cbf
Bryan-Rose23 <56421942+Bryan-Rose23@users.noreply.github.com>
2024-11-26 19:45:10 -0600

    adding executes file

---
+++ TRANSACT-SQL/EXECUTES.sql
@@ -0,0 +1,13 @@
+EXEC PCDINSERTAR_CARGO 'Gerente';
+EXEC PCDINSERTAR_CARGO 'Coordinador de Logística y Mercadeo';
+
+EXEC PCDINSERTAR_DEPARTAMENTO 'Gerencia';
+EXEC PCDINSERTAR_DEPARTAMENTO 'Logistica y Mercadeo';
+
+EXEC PCDINSERTAR_GRUPO 'Administrador';
+EXEC PCDINSERTAR_GRUPO 'Gerencia';
+EXEC PCDINSERTAR_GRUPO 'Logística y Mercadeo';
+
+EXEC PCDINSERTAR_EMPLEADO 'William', 'Jefferson', 'Gaitan', 'Rostran', '0010708000020K', 'Bo. Rene Polanco, de donde fue la casa de la mujer 1/2 C al Norte', 77748181, 'wgaitan00@gmail.com', 1, 1;
+
+EXEC PCDINSERTAR_MENU 'Inicio', 'Home/Index', 0;
